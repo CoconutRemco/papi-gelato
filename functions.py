16845c072603269727b03f8cae8e0f630414b63e
CoconutRemco <112873034+CoconutRemco@users.noreply.github.com>
2022-12-14 11:39:22 +0100

    Andere manier van gebruik van functions

--- functions.py
+++ functions.py
@@ -3,38 +3,9 @@ def showintro():
 
 def error():
     print("Sorry, dat snap ik niet.")
-def stap1(aantalbolletjes:int):
-    if aantalbolletjes >3:
-        return("Dan krijgt u van mij een bakje met ",aantalbolletjes,' bolletjes.')
-    elif aantalbolletjes >0 and aantalbolletjes <4:
-        pass
-    elif aantalbolletjes >8:
-        return('Sorry, zulke grote bakken hebben we niet.')
-    else:
-        return error()
 
-        
-def stap2(bakjeofhoorntje:str):
-    if bakjeofhoorntje == 'bakje':
-        return 'stap3'
-
-    elif bakjeofhoorntje == 'hoorntje':
-        return 'stap3'
-    else:
-        return error()
-        
-
-
-def stap3(meerbestellen:str,aantalbolletjes:int,bakjeofhoorntje:str):
-    if meerbestellen == 'ja':
-        return 'stap1'
-    elif meerbestellen == 'nee':
-        return print("Bedankt en tot ziens!")
-    else:
-        return error()
-
-
-
+def errorbakje():
+    print("Sorry, een bakje kan maximaal 8 bolletjes bevatten.")
 
 
 
--- papigelatoflow.py
+++ papigelatoflow.py
@@ -1,26 +1,52 @@
-from functions import stap1,stap2,stap3,error,showintro
-opnieuw =True
-bestelling=[]
-while opnieuw == True:
-    aantalbolletjes =int(input('Hoeveel bolletjes wilt u?'))
-    if aantalbolletjes >3:
-        bakjeofhoorntje='bakje'
-        print(stap1(aantalbolletjes))
-    elif aantalbolletjes <4 and aantalbolletjes <9:
-        bakjeofhoorntje= str(input("Wilt u deze "+str(aantalbolletjes)+" bolletje(s) in een hoorntje of een bakje?")).lower()
-    else:
+from functions import error,showintro,errorbakje
+nognkeer = True
+showintro()
+while nognkeer == True:
+    try:
+        bolletjes = int(input('Hoeveel bolletjes wilt u? '))
+        if bolletjes >= 1 and bolletjes <= 3:
+            mnognkeer = False
+            aantalbolletjes = bolletjes
+            bakje = input('Wilt u deze '+ str(bolletjes) +' bolletje(s) in een hoorntje of een bakje? ').lower()
+            if bakje == 'bakje':
+                meerbestellen = input('Hier is uw bakje met '+ str(bolletjes) +' bolletje(s). Wilt u nog meer bestellen?').lower()
+                if meerbestellen == 'ja':
+                    nognkeer = True
+                elif meerbestellen  == 'nee':
+                    print('Bedankt en tot ziens!')
+                    nognkeer = False
+                else:
+                    error()
+            elif bakje == 'hoorntje':
+                meerbestellen = input('Hier is uw hoorntje met '+ str(bolletjes) +' bolletje(s). Wilt u nog meer bestellen?').lower()
+                if meerbestellen == 'ja':
+                    nognkeer = True
+                elif meerbestellen == 'nee':
+                    nognkeer = False
+                    print('Bedankt en tot ziens!')
+                else:
+                    error()
+            else:
+                error()
+
+        elif bolletjes >= 4 and bolletjes <= 8:
+            aantalbolletjes = bolletjes
+            print('Dan krijgt u van mij een bakje met '+ str(bolletjes) +' bolletjes.')
+            nognkeer = False
+            meerbestellen = input('Wilt u nog meer bestellen?').lower()
+            if meerbestellen == 'ja':
+                nognkeer = True
+            elif meerbestellen == 'nee':
+                nognkeer = False
+                print('Bedankt en tot ziens!')
+            else:
+                error()
+
+        elif bolletjes > 8:
+            errorbakje()
+            nognkeer= True
+        elif bolletjes == 0 or bolletjes <0:
+            error()
+            nognkeer = True
+    except:
         error()
-    if stap2(bakjeofhoorntje) == 'stap3':
-        print("Hier is uw ",bakjeofhoorntje," met ",aantalbolletjes," bolletje(s).")
-        meerbestellen= str(input("Wilt u nog meer bestellen?")).lower()
-        
-        if stap3(meerbestellen,aantalbolletjes,bakjeofhoorntje) == 'stap1':
-            opnieuw=True
-            bestelling.append("Hier is uw "+str(bakjeofhoorntje)+" met "+str(aantalbolletjes)+" bolletje(s).")
-
-        else:
-            opnieuw=False
-            print(stap3(meerbestellen,aantalbolletjes,bakjeofhoorntje))
-            bestelling.append("Hier is uw "+str(bakjeofhoorntje)+" met "+str(aantalbolletjes)+" bolletje(s).")
-            print(bestelling)
-
