30634ab0f000d8a0fbcb4489dc0c1e2cb8e48753
CoconutRemco <112873034+CoconutRemco@users.noreply.github.com>
2022-12-19 11:55:25 +0100

    new features

--- test.py
+++ test.py
@@ -1,26 +1,24 @@
 from papigelatofunctions import *
 herhalen =  True
 showintro()
-bestellingtotaal = []
 while herhalen == True:
     bolletjes=aantalbolletjes()
     if bolletjes >4:
         print("Dan krijgt u van mij een bakje met "+str(bolletjes)+" bolletjes.")
         verpakkingbolletjes = 'bakje'
-        nognkeer=meerbestellen()
-        if nognkeer == True:
-            herhalen = True
-        else:
-            herhalen = False
     else:
         verpakkingbolletjes = verpakking(bolletjes)
         print("Hier is uw "+str(verpakkingbolletjes)+" met "+str(bolletjes)+" bolletje(s).")
-        nognkeer=meerbestellen()
-        if nognkeer == True:
-            herhalen = True
-        else:
-            herhalen = False
-    bestellingtotaal.append(bestelling(bolletjes,verpakkingbolletjes))
-print(bestellingtotaal)
+    nognkeer=meerbestellen()
+    if nognkeer == True:
+        herhalen = True
+    else:
+        herhalen = False
+bonnetje(bolletjes,verpakkingbolletjes)
+totziens()
+
+
+
+
 
     
